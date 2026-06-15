fe415cb3058e8cc44de69f2285983fa1a409b6dc
raushank7460 <raushank7460@gmail.com>
2026-06-15 11:47:28 +0530

    add someting

--- PsudoCode/Day2.java
+++ PsudoCode/Day2.java
@@ -14,17 +14,44 @@ public class Day2 {
     //     System.out.println("left"+left);
     //     System.out.println("right"+right);
     // }
-    public static void main(String[] args) {
-        int a=12;
-        int b=7;
-        int c=5;
-        int x=(a^b)+(c<<1);
-        int y=(a&b)*(b|c);
-        int res=(x>y)?x-y:y-x;
-        System.out.println("X="+ x);
-        System.out.println("y="+ y);
-        System.out.println("res="+ res);
+    // public static void main(String[] args) {
+    //     int a=12;
+    //     int b=7;
+    //     int c=5;
+    //     int x=(a^b)+(c<<1);
+    //     int y=(a&b)*(b|c);
+    //     int res=(x>y)?x-y:y-x;
+    //     System.out.println("X="+ x);
+    //     System.out.println("y="+ y);
+    //     System.out.println("res="+ res);
+    // }
+public static void main(String[] args) {
+    // for(int i=1;i<=4;i++){
+    //     for(int j=1;j<=4;j++){
+    //         System.out.print(j+" ");
+    //     }
+    //     System.out.println();
+    // }
+//     for(int i=1,j=10;i<j;i++,j--){
+//         System.out.println(i+" "+j);
+// // time complexity: n/2
+//     }
+// for(int i=1;i<=10;i++){
+//     System.out.println(i+" ");
+//     if(i%3==0){
+//         i++;
+//     }
+// }
+int count=0;
+    for(int i=1;i<=3;i++){
+        for(int j=1;j<=2;j++){
+            for(int k=1;k<=2;k++){
+                count++;
+            }
+        }
     }
+    System.out.println(count);
+}
     
 }
 
